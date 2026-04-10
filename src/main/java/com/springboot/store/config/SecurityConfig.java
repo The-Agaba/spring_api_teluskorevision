91bc9f3042062ddd1297f83e8e4eb757c208d819
The-Agaba <collin.a.raymund@gmail.com>
2026-04-11 01:31:19 +0300

    revisiting security rules

--- src/main/java/com/springboot/store/config/SecurityConfig.java
+++ src/main/java/com/springboot/store/config/SecurityConfig.java
@@ -75,9 +75,16 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider(userDetailsService, passwordEncoder()))
                 .authorizeHttpRequests(
                         c-> c
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/swagger-ui.html").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 .requestMatchers("/api/carts/**").permitAll()
                                 .requestMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
                                 .requestMatchers(HttpMethod.POST,"/api/users").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/products/**").hasRole(Role.ADMIN.name())
+                                .requestMatchers(HttpMethod.PUT,"/api/products/**").hasRole(Role.ADMIN.name())
+                                .requestMatchers(HttpMethod.DELETE,"/api/products/**").hasRole(Role.ADMIN.name())
+                                .requestMatchers(HttpMethod.GET,"/api/products/**").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/api/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/api/auth/refresh").permitAll()
                                 .requestMatchers(HttpMethod.POST,"/api/checkout/webhook").permitAll()
